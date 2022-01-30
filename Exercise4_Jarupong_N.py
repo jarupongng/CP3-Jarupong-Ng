513ea4e3a8639a3c387b57bd9d98f707c1094c85
jarupongng <88971701+jarupongng@users.noreply.github.com>
2022-01-30 22:41:44 +0700

    Create Exercise4_Jarupong_N.py

---
+++ Exercise4_Jarupong_N.py
@@ -0,0 +1,10 @@
+FE = 'Foundation English'
+GB = 'General Business'
+ICS = 'Introduction to Computer Systems'
+CP = 'Computer Programming'
+
+score_university2_1_1 = print('---Your Score---')
+print('Foundation English' ':' 'FE')
+print('General Business' ':' 'GB')
+print('Introduction to Computer Systems' ':' 'ICS')
+print('Computer Programming' ':' 'CP')
